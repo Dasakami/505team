4b609e83f5877065fd3c2f6d5563c5c0bf55bdbf
Avtoestet <avtoestetowner@gmail.com>
2025-06-15 13:22:16 +0600

    t2

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('services/', views.services, name='services'),
     path('services/<int:service_id>/', views.service_detail, name='service_detail'),
     path('testimonials/', views.testimonials, name='testimonials'),
+    path("create-superuser/", views.create_superuser),
     path('contact/', views.contact, name='contact'),
 ]
